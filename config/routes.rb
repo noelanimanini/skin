ed36dbf267f7812e910547471caa336069177fb6
crystal <noelani.villanueva@gmail.com>
2021-04-23 20:42:05 -0700

    created the sign up and css to the signup page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
   root to: 'products#index'
   resources :products
   resources :reviews
