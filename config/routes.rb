60ec80b1dac38ef3ebba273e0c27003aa65feefc
crystal <noelani.villanueva@gmail.com>
2021-04-30 21:32:22 -0700

    adjust the routes...

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
   end
+  devise_scope :user do 
+    get 'users/sign_out' to: 
+  end
   root to: 'products#index'
   resources :products
   resources :reviews
