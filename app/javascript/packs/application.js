86a15c33932165b019f4f2c7c4d99d139286d3d0
crystal <noelani.villanueva@gmail.com>
2021-05-02 15:08:43 -0700

    added jquery since rails 6 doesnt support it anymore and abstracted javascript with webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,11 @@
 // that code so it'll be compiled.
 //= require jquery
 //= require bootstrap-sprockets
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
-import "products";
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("jquery");
+require("products/style");
 
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
+console.log("hello??");
